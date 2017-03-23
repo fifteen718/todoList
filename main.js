209f28b14a42b216095260eed8c02c86f78d6398
MisterKay <756165959@qq.com>
2017-03-23 23:20:04 +0800

    新增手机端适配
    1、判断web访问端来源，对手机端进行跳转
    2、新增手机端indexPhone.html
    3、有待完善：动画效果、手机滑动响应

---
+++ main.js
@@ -0,0 +1,55 @@
+const STORAGE_KEY = 'todos-vuejs';
+fetch = function () {
+  return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '[]')
+};
+save = function (items) {
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items))
+};
+
+new Vue({
+  el: '#app',
+
+  data: function () {
+    return {
+      title: 'Todo List',
+      items: fetch(),
+      newItem: ''
+    }
+  },
+
+  watch: {
+    items: {
+      handler: function (items) {
+        save(items);
+      },
+      deep: true
+    },
+  },
+
+  methods: {
+    toggleFinish: function (item) {
+      item.isFinished = !item.isFinished;
+    },
+    addNew: function () {
+      if (this.newItem.match(/^[ ]*$/)) {
+        alert("添加内容不能为空！");
+        this.newItem = '';
+        return false;
+      }
+      this.items.unshift({
+        label: this.newItem,
+        isFinished: false
+      });
+      this.newItem = ''
+    },
+    clearAll: function () {
+      window.localStorage.clear();
+      this.items = [];
+    },
+    deleteOne: function (index) {
+      event.stopPropagation();
+      this.items.splice(index, 1);
+    }
+  }
+  
+});
