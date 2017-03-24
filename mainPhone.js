ee8fc1fa4be4954fae843237338b96d05c746887
MisterKay <756165959@qq.com>
2017-03-24 15:38:04 +0800

    新增 mainPhone.js
    手机端适配滑动事件
    左滑显示删除按钮
    右滑影藏删除按钮
    （第一次尝试监听触屏事件，有点意思~~）

---
+++ mainPhone.js
@@ -0,0 +1,78 @@
+const STORAGE_KEY = 'todos-vuejs';
+fetch = function () {
+  return JSON.parse(window.localStorage.getItem(STORAGE_KEY) || '[]')
+};
+save = function (items) {
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(items))
+};
+
+new Vue({
+  el: '#app',
+
+  data: function () {
+    return {
+      title: 'Todo List',
+      items: fetch(),
+      newItem: ''
+    }
+  },
+
+  watch: {
+    items: {
+      handler: function (items) {
+        save(items);
+      },
+      deep: true
+    }
+  },
+
+  methods: {
+    toggleFinish: function (item) {
+      item.isFinished = !item.isFinished;
+    },
+    addNew: function () {
+      if (this.newItem.match(/^[ ]*$/)) {
+        alert("添加内容不能为空！");
+        this.newItem = '';
+        return false;
+      }
+      this.items.unshift({
+        label: this.newItem,
+        deleteShow: false,
+        isFinished: false
+      });
+      this.newItem = ''
+    },
+    clearAll: function () {
+      window.localStorage.clear();
+      this.items = [];
+    },
+    deleteOne: function (index) {
+      if (window.event && event.stopPropagation) {
+        event.stopPropagation();
+      }
+      this.items.splice(index, 1);
+    },
+    touchstart: function () {
+      var e = window.event;
+      // e.preventDefault();
+      startX = e.targetTouches[0].pageX;
+      startY = e.targetTouches[0].pageY;
+    },
+    touchmove: function (index, item) {
+      var e = window.event;
+      // e.preventDefault();
+      moveEndX = e.targetTouches[0].pageX;
+      moveEndY = e.targetTouches[0].pageY;
+      X = moveEndX - startX;
+      Y = moveEndY - startY;
+      if (Math.abs(X) > 120 && Math.abs(X) > Math.abs(Y) && X > 0) {
+        console.log("右滑事件");
+        item.deleteShow = false;
+      } else if (Math.abs(X) > 120 && Math.abs(X) > Math.abs(Y) && X < 0) {
+        console.log("左滑事件");
+        this.items[index].deleteShow = true;
+      }
+    }
+  }
+});
